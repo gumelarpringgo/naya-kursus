8e36d0f1fb407873bffd5baae2365ce78c6aec5d
gumelarpringgo <gumelarpringgo.media@gmail.com>
2023-01-18 14:34:55 +0700

    day 2 operator

---
+++ 02-01/practice/sumOfPositive/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func PositiveSum(numbers []int) int {
+	var x int
+	for _, num := range numbers {
+		if num > 0 {
+			x += num
+		}
+	}
+	return x
+}
+
+func main() {
+	numbers := []int{1, 3, 5, -3, 4, -7}
+	fmt.Println(PositiveSum(numbers))
+}
